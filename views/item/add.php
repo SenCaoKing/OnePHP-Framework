02d999a6a2cc3869c40365ba646230c19b2e37fe
SenCaoKing <1134349927@qq.com>
2019-01-07 21:38:45 +0800

    框架开发 - 修改调试页面

--- views/item/add.php
+++ views/item/add.php
@@ -1,1 +1,1 @@
-<a class="big" href="../item/viewall">Todo添加成功，点击返回</a>
+<a class="big" href="../item/">添加成功，点击返回</a>
--- views/item/index.php
+++ views/item/index.php
@@ -1,7 +1,9 @@
-<form action="../item/add.php" method="post">
-    <input type="text" value="I have to..." onclick="this.value=''" name="todo"> <input type="submit" value="add">
+<form action="../item/add" method="post">
+    <input type="text" value="I have to..." onclick="this.value=''" name="value">
+    <input type="submit" value="添加">
 </form>
 <br /><br />
+
 <?php $number = 0?>
 
 <?php foreach ($todo as $todoitem):?>
@@ -10,5 +12,8 @@
             <?php echo ++$number?>
             <?php echo $todoitem['Item']['item_name']?>
         </span>
-    </a><br/>
+    </a>
+    ----
+    <a class="big" href="../item/delete/<?php $todoitem['Item']['id']?>">删除</a>
+    <br/>
 <?php endforeach;?>
