c23460740d610eb6ab029a608b8c7cb1c7c8b938
SenCaoKing <1134349927@qq.com>
2019-01-09 19:51:54 +0800

    框架开发 - 删除APP_URL

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 
 define('APP_PATH', __DIR__ . '/'); // 应用目录为当前目录
 define('APP_DEBUG', true); // 开启调试模式
-define('APP_URL', 'http://www.onephp.test'); // 网站根URL
-
-
 require(APP_PATH . 'onephp/Onephp.php'); // 加载框架
 
 $config = require(APP_PATH . 'config/config.php');
